8db8533562953f034ecdb56b2c31e805a50264ef
tommyb82 <thomasbunting@gmail.com>
2020-05-08 14:04:07 +0100

    WIP - add Support node type

---
+++ src/main/java/uk/gov/crowncommercial/dts/scale/service/gm/model/ogm/Support.java
@@ -0,0 +1,17 @@
+package uk.gov.crowncommercial.dts.scale.service.gm.model.ogm;
+
+/**
+ *
+ */
+public class Support implements QuestionInstanceOutcome {
+
+  /* (non-Javadoc)
+   * @see uk.gov.crowncommercial.dts.scale.service.gm.model.ogm.QuestionInstanceOutcome#getUuid()
+   */
+  @Override
+  public String getUuid() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
