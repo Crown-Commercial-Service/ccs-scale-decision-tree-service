0f5768606289ce7f1312abf3a6ff49288d8955f6
tommyb82 <thomasbunting@gmail.com>
2020-06-04 00:13:33 +0100

    Add OPTIONAL MATCH to Lot from Agreement on multi and CI repo queries

--- src/main/java/uk/gov/crowncommercial/dts/scale/service/gm/repository/OutcomeRepository.java
+++ src/main/java/uk/gov/crowncommercial/dts/scale/service/gm/repository/OutcomeRepository.java
@@ -26,8 +26,9 @@ public interface OutcomeRepository extends Neo4jRepository<QuestionInstanceOutco
   @Query("MATCH (q:QuestionInstance {uuid: $currentQstnUuid})-[:HAS_ANSWER_GROUP]->(:AnswerGroup)-[:HAS_MULTI_SELECT]-(:MultiSelect {uuid: $multiSelectUuid})-[:HAS_OUTCOME]->(outcome) "
       + "OPTIONAL MATCH (outcome)-[r:DEFINED_BY]->(qd:Question) "
       + "OPTIONAL MATCH (outcome)-[rnag:HAS_ANSWER_GROUP]->(nag:AnswerGroup) "
+      + "OPTIONAL MATCH (outcome:Agreement)-[rlot:HAS_LOT]->(lot:Lot) "
       + "OPTIONAL MATCH (nag)-[na:HAS_ANSWER]->(a:Answer)-[hci:HAS_CONDITIONAL_INPUT]->(ciq:Question)  "
-      + "RETURN outcome, r, qd, rnag, nag, na, a, hci, ciq")
+      + "RETURN outcome, r, qd, rnag, nag, na, a, hci, ciq, rlot, lot")
   List<QuestionInstanceOutcome> findMultiAnswerOutcomes(
       @Param("currentQstnUuid") String currentQstnUuid,
       @Param("multiSelectUuid") String multiSelectUuid);
@@ -49,8 +50,9 @@ public interface OutcomeRepository extends Neo4jRepository<QuestionInstanceOutco
       + "AND ha.lowerBoundInclusive <= $answerValue AND ha.upperBoundExclusive > $answerValue "
       + "OPTIONAL MATCH (outcome)-[r:DEFINED_BY]->(qd:Question) "
       + "OPTIONAL MATCH (outcome)-[rnag:HAS_ANSWER_GROUP]->(nag:AnswerGroup) "
+      + "OPTIONAL MATCH (outcome:Agreement)-[rlot:HAS_LOT]->(lot:Lot) "
       + "OPTIONAL MATCH (nag)-[na:HAS_ANSWER]->(a:Answer)  "
-      + "RETURN outcome, r, qd, rnag, nag, na, a")
+      + "RETURN outcome, r, qd, rnag, nag, na, a, rlot, lot")
   List<QuestionInstanceOutcome> findSingleConditionalNumericAnswerOutcomes(
       @Param("currentQstnUuid") String currentQstnUuid, @Param("answerUuid") String answerUuid,
       @Param("answerValue") Double answerValue);
