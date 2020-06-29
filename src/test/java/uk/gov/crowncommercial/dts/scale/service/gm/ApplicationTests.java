c3de7612b69b255a8f41cffa94833c0dad5987f5
Tom Bunting <6031257+tommyb82@users.noreply.github.com>
2020-06-29 23:09:37 +0100

    Bugfix/sfat 242 dropped db conns (#19)
    * Connection pool management for Neo4J
    
    * Direct configuration of Bolt driver for liveness checking on every usage
    
    Alos bump SpringBoot version to latest
    
    * Fix the test suite

--- src/test/java/uk/gov/crowncommercial/dts/scale/service/gm/ApplicationTests.java
+++ src/test/java/uk/gov/crowncommercial/dts/scale/service/gm/ApplicationTests.java
@@ -1,9 +1,12 @@
 package uk.gov.crowncommercial.dts.scale.service.gm;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@DataNeo4jTest
 class ApplicationTests {
 
   @Test
